8699d7dfda9e40a7d69cdf7e45ec493a1841e7c9
evanalifian <evanalifian@gmail.com>
2026-05-04 20:53:01 +0700

    docs: update homepage introduction text

--- src/View/index.php
+++ src/View/index.php
@@ -1,8 +1,8 @@
 <div class="container">
   <div class="row justify-content-center pt-5 mt-5">
     <div class="col-sm-6 text-center">
-      <h1>PHP Boilerplate</h1>
-      <p>A starter template to build website using native PHP.</p>
+      <h1>Pinnio</h1>
+      <p>Aplikasi social media berbasi web untuk mengunggah meme untuk kalangan mahasiswa.</p>
       <p>Try authentication feature <a href="/login">here</a>.</p>
     </div>
   </div>
